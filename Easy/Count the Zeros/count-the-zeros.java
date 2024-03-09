c0c16714eafc44dbd43e9279fbb852a7be99953b
Raunak0713 <100823919+Raunak0713@users.noreply.github.com>
2024-03-09 17:28:10 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count the Zeros/count-the-zeros.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().countZeroes(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int countZeroes(int[] arr, int n) {
+        int start = 0, end = n-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]==0) end = mid - 1; 
+            else if(arr[mid]>0) start = mid+1;
+        }
+        return n-1-end;
+    }
+
+}
