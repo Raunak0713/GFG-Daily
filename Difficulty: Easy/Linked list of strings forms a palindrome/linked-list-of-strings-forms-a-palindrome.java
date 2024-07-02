d9e6844abb490bc2b2ada09c64a52da2f2045e49
Raunak0713 <100823919+Raunak0713@users.noreply.github.com>
2024-07-02 18:19:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Linked list of strings forms a palindrome/linked-list-of-strings-forms-a-palindrome.java
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    String data;
+    Node next;
+
+    Node(String x) {
+        data = x;
+        next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    LinkedList() { head = null; }
+
+    void addNode(String str) {
+        if (head == null) {
+            head = new Node(str);
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = new Node(str);
+        }
+    }
+
+    void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int K = sc.nextInt();
+            LinkedList list = new LinkedList();
+
+            for (int i = 0; i < K; i++) {
+                String str = sc.next();
+                list.addNode(str);
+            }
+
+            Solution ob = new Solution();
+            boolean ans = ob.compute(list.head);
+            if (ans) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+        sc.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    StringBuilder sb;
+    public boolean compute(Node root) {
+        sb = new StringBuilder();
+        Node curr = root;
+        while(curr != null){
+            sb.append(curr.data);
+            curr = curr.next;
+        }
+        return Check(sb.toString());
+    }
+    public boolean Check(String s){
+        int n = s.length();
+        for(int i=0; i<n/2; i++){
+            if(s.charAt(i) != s.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
