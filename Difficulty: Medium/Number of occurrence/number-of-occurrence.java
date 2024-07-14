d9d5e262e828fab7239800bf93ec58543c49e1eb
Raunak0713 <100823919+Raunak0713@users.noreply.github.com>
2024-07-14 14:22:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of occurrence/number-of-occurrence.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().count(arr, n, x);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    int count(int[] arr, int n, int x) {
+        int firstPos = findFirstPosition(arr, x);
+        if (firstPos == -1) {
+            return 0; // If the first position is -1, the target is not in the array
+        }
+        int lastPos = findLastPosition(arr, x);
+        return lastPos - firstPos + 1;
+    }
+
+    int findFirstPosition(int[] nums, int target){
+        int index = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] >= target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+            if (nums[mid] == target) index = mid;
+        }
+        return index;
+    }
+
+    int findLastPosition(int[] nums, int target){
+        int index = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            if (nums[mid] == target) index = mid;
+        }
+        return index;
+    }
+}
